89c40ca2593abb4f441a2948ced6e3c99e91912b
KOFF63 <Kriloff63@yandex.ru>
2025-11-24 20:42:42 +0400

    2 GW

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'materials',  # ← добавляем наше приложение   !!!!!!!!!!!!!!!
+    'materials',  # ←  подключаем приложение   !!!!!!!!!!!!!!!
 ]
 
 MIDDLEWARE = [
